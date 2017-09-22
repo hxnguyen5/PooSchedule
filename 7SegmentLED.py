4617bea99798ec47e3d2125d96b7065de1df18e6
hxnguyen5 <32071742+hxnguyen5@users.noreply.github.com>
2017-09-22 13:35:06 -0700

    Create 7SegmentLED.py

---
+++ 7SegmentLED.py
@@ -0,0 +1,178 @@
+import time
+import RPi.GPIO as GPIO
+
+#GPIO setup
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(29, GPIO.OUT) #G  
+GPIO.setup(31, GPIO.OUT) #F
+GPIO.setup(33, GPIO.OUT)#A
+GPIO.setup(35, GPIO.OUT)#B
+GPIO.setup(37, GPIO.OUT)#E
+GPIO.setup(32, GPIO.OUT)#D
+GPIO.setup(36, GPIO.OUT)#C
+GPIO.setup(38, GPIO.OUT)#P
+
+#Numbers 0-9
+
+def Zero():
+    GPIO.output(29, False)#G
+    GPIO.output(31, True)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, True)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, True)#P
+    
+
+def One():
+    GPIO.output(29, False)#G
+    GPIO.output(31, False)#F
+    GPIO.output(33, False)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, False)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, True)#P
+def Two():
+    GPIO.output(29, True)#G
+    GPIO.output(31, False)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, True)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, False)#C
+    GPIO.output(38, True)#P
+def Three():
+    GPIO.output(29, True)#G
+    GPIO.output(31, False)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, True)#P
+def Four():
+    GPIO.output(29, True)#G
+    GPIO.output(31, True)#F
+    GPIO.output(33, False)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, False)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, False)#P
+def Five():
+    GPIO.output(29, True)#G
+    GPIO.output(31, True)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, False)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, False)#P
+def Six():
+    GPIO.output(29, True)#G
+    GPIO.output(31, True)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, False)#B
+    GPIO.output(37, True)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, True)#P
+def Seven():
+    GPIO.output(29, False)#G
+    GPIO.output(31, False)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, False)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, False)#P
+def Eight():
+    GPIO.output(29, True)#G
+    GPIO.output(31, True)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, True)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, True)#P
+def Nine():
+    GPIO.output(29, True)#G
+    GPIO.output(31, True)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, True)#P
+    
+#Stop when finished
+def Stop():
+    GPIO.output(29, False)#G
+    GPIO.output(31, False)#F
+    GPIO.output(33, False)#A
+    GPIO.output(35, False)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, False)#D
+    GPIO.output(36, False)#C
+    GPIO.output(38, False)#P    
+
+while True:    
+    
+    KeyPress = input("Type a number from 0-9: ")
+    
+    if KeyPress == "0":
+        print("0 is showing")
+        Zero()
+        time.sleep(0.65)
+        print("Type a number from 0-9")
+    elif KeyPress == "1":
+        print("1 is showing")
+        One()
+        time.sleep(0.65)
+        print("Type a number from 0-9")
+    elif KeyPress =="2":
+        print("2 is showing")
+        Two()
+        time.sleep(0.65)
+        print("Type a number from 0-9")
+    elif KeyPress == "3":
+        print("3 is showing")
+        Three()
+        time.sleep(0.65)
+        print("Type a number from 0-9")
+    elif KeyPress == "4":
+        print("4 is showing")
+        Four()
+        time.sleep(0.65)
+        print("Type a number from 0-9")
+    elif KeyPress == "5":
+        print("5 is showing")
+        Five()
+        time.sleep(0.65)
+        print("Type a number from 0-9")
+    elif KeyPress == "6":
+        print("6 is showing")
+        Six()
+        time.sleep(0.65)
+        print("Type a number from 0-9")
+    elif KeyPress == "7":
+        print("7 is showing")
+        Seven()
+        time.sleep(0.65)
+        print("Type a number from 0-9")
+    elif KeyPress == "8":
+        print("8 is showing")
+        Eight()
+        time.sleep(0.65)
+        print("Type a number from 0-9")
+    elif KeyPress == "9":
+        print("9 is showing")
+        Nine()
+        time.sleep(0.65)
+        print("Type a number from 0-9")
+    else:
+        Stop()
+        print("Type a number from 0-9")
