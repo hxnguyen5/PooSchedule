248d9ce73c06f1006738296f0a46491ecdc06859
hxnguyen5 <32071742+hxnguyen5@users.noreply.github.com>
2017-09-21 20:07:53 -0700

    Update PooSchedule.py

--- PooSchedule.py
+++ PooSchedule.py
@@ -1,126 +1,124 @@
 import time
 import RPi.GPIO as GPIO
-from espeak import espeak
-from datetime import datetime
 from subprocess import check_output
 
 #GPIO setup
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(5, GPIO.OUT) #G  
-GPIO.setup(6, GPIO.OUT) #F
-GPIO.setup(13, GPIO.OUT)#A
-GPIO.setup(19, GPIO.OUT)#B
-GPIO.setup(26, GPIO.OUT)#E
-GPIO.setup(16, GPIO.OUT)#D
-GPIO.setup(20, GPIO.OUT)#C
-GPIO.setup(21, GPIO.OUT)#P
+GPIO.setup(29, GPIO.OUT) #G  
+GPIO.setup(31, GPIO.OUT) #F
+GPIO.setup(33, GPIO.OUT)#A
+GPIO.setup(35, GPIO.OUT)#B
+GPIO.setup(37, GPIO.OUT)#E
+GPIO.setup(32, GPIO.OUT)#D
+GPIO.setup(36, GPIO.OUT)#C
+GPIO.setup(38, GPIO.OUT)#P
 
 #Numbers 0-9
 
 def Zero():
-    GPIO.output(5, False)#G
-    GPIO.output(6, True)#F
-    GPIO.output(13, True)#A
-    GPIO.output(19, True)#B
-    GPIO.output(26, True)#E
-    GPIO.output(16, True)#D
-    GPIO.output(20, True)#C
-    GPIO.output(21, True)#P
+    GPIO.output(29, False)#G
+    GPIO.output(31, True)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, True)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, True)#P
     
 
 def One():
-    GPIO.output(5, False)#G
-    GPIO.output(6, False)#F
-    GPIO.output(13, False)#A
-    GPIO.output(19, True)#B
-    GPIO.output(26, False)#E
-    GPIO.output(16, False)#D
-    GPIO.output(20, True)#C
-    GPIO.output(21, True)#P
+    GPIO.output(29, False)#G
+    GPIO.output(31, False)#F
+    GPIO.output(33, False)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, False)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, True)#P
 def Two():
-    GPIO.output(5, True)#G
-    GPIO.output(6, False)#F
-    GPIO.output(13, True)#A
-    GPIO.output(19, True)#B
-    GPIO.output(26, True)#E
-    GPIO.output(16, True)#D
-    GPIO.output(20, False)#C
-    GPIO.output(21, True)#P
+    GPIO.output(29, True)#G
+    GPIO.output(31, False)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, True)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, False)#C
+    GPIO.output(38, True)#P
 def Three():
-    GPIO.output(5, True)#G
-    GPIO.output(6, False)#F
-    GPIO.output(13, True)#A
-    GPIO.output(19, True)#B
-    GPIO.output(26, False)#E
-    GPIO.output(16, True)#D
-    GPIO.output(20, True)#C
-    GPIO.output(21, True)#P
+    GPIO.output(29, True)#G
+    GPIO.output(31, False)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, True)#P
 def Four():
-    GPIO.output(5, True)#G
-    GPIO.output(6, True)#F
-    GPIO.output(13, False)#A
-    GPIO.output(19, True)#B
-    GPIO.output(26, False)#E
-    GPIO.output(16, False)#D
-    GPIO.output(20, True)#C
-    GPIO.output(21, False)#P
+    GPIO.output(29, True)#G
+    GPIO.output(31, True)#F
+    GPIO.output(33, False)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, False)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, False)#P
 def Five():
-    GPIO.output(5, True)#G
-    GPIO.output(6, True)#F
-    GPIO.output(13, True)#A
-    GPIO.output(19, False)#B
-    GPIO.output(26, False)#E
-    GPIO.output(16, True)#D
-    GPIO.output(20, True)#C
-    GPIO.output(21, False)#P
+    GPIO.output(29, True)#G
+    GPIO.output(31, True)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, False)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, False)#P
 def Six():
-    GPIO.output(5, True)#G
-    GPIO.output(6, True)#F
-    GPIO.output(13, True)#A
-    GPIO.output(19, False)#B
-    GPIO.output(26, True)#E
-    GPIO.output(16, True)#D
-    GPIO.output(20, True)#C
-    GPIO.output(21, True)#P
+    GPIO.output(29, True)#G
+    GPIO.output(31, True)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, False)#B
+    GPIO.output(37, True)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, True)#P
 def Seven():
-    GPIO.output(5, False)#G
-    GPIO.output(6, False)#F
-    GPIO.output(13, True)#A
-    GPIO.output(19, True)#B
-    GPIO.output(26, False)#E
-    GPIO.output(16, False)#D
-    GPIO.output(20, True)#C
-    GPIO.output(21, False)#P
+    GPIO.output(29, False)#G
+    GPIO.output(31, False)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, False)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, False)#P
 def Eight():
-    GPIO.output(5, True)#G
-    GPIO.output(6, True)#F
-    GPIO.output(13, True)#A
-    GPIO.output(19, True)#B
-    GPIO.output(26, True)#E
-    GPIO.output(16, True)#D
-    GPIO.output(20, True)#C
-    GPIO.output(21, True)#P
+    GPIO.output(29, True)#G
+    GPIO.output(31, True)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, True)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, True)#P
 def Nine():
-    GPIO.output(5, True)#G
-    GPIO.output(6, True)#F
-    GPIO.output(13, True)#A
-    GPIO.output(19, True)#B
-    GPIO.output(26, False)#E
-    GPIO.output(16, True)#D
-    GPIO.output(20, True)#C
-    GPIO.output(21, True)#P
+    GPIO.output(29, True)#G
+    GPIO.output(31, True)#F
+    GPIO.output(33, True)#A
+    GPIO.output(35, True)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, True)#D
+    GPIO.output(36, True)#C
+    GPIO.output(38, True)#P
     
 #Stop when finished
 def Stop():
-    GPIO.output(5, False)#G
-    GPIO.output(6, False)#F
-    GPIO.output(13, False)#A
-    GPIO.output(19, False)#B
-    GPIO.output(26, False)#E
-    GPIO.output(16, False)#D
-    GPIO.output(20, False)#C
-    GPIO.output(21, False)#P    
+    GPIO.output(29, False)#G
+    GPIO.output(31, False)#F
+    GPIO.output(33, False)#A
+    GPIO.output(35, False)#B
+    GPIO.output(37, False)#E
+    GPIO.output(32, False)#D
+    GPIO.output(36, False)#C
+    GPIO.output(38, False)#P    
 
 
 print("Type a number from 0-9")
