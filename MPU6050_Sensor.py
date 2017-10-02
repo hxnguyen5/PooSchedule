bdaadb3622a4bdf019b95f88e8092eff82c2f61b
hxnguyen5 <32071742+hxnguyen5@users.noreply.github.com>
2017-10-02 08:51:45 -0700

    Create MPU6050_Sensor.py

---
+++ MPU6050_Sensor.py
@@ -0,0 +1,3 @@
+# This python code is used with an MPU 6050 6-DOF sensor
+# This sensor has a 3-axis gyroscope and a 3-axis accelerometer, i.e. IMU
+# Typical application is for self-balancing robot or self flying quadcopter and airplane.
