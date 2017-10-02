fee9ac8c515240c5ce3cc64bff0f1865b11e6fda
hxnguyen5 <32071742+hxnguyen5@users.noreply.github.com>
2017-10-02 08:49:20 -0700

    Create GY-86_10DOF_Sensor.py

---
+++ GY-86_10DOF_Sensor.py
@@ -0,0 +1,5 @@
+# This python code is used for the GY-86 10-DOF Sensor
+# This sensor has a MPU6050, a 6-DOF Gyro and Accelerometer
+# A HMC5883L 3-DOF Magnetometer
+# and a MS5611 Barometer
+# Typical application for sensor of this type is for self-balancing robot or self-flying quad-copter and airplane.
